b133928efe1b6849fc792e95a187352a65f68382
Shishir Kakaraddi <shishirkakaraddi@gmail.com>
2012-02-17 18:44:56 -0500

    Problem 2

---
+++ problem_2.rb
@@ -0,0 +1,21 @@
+n1 = 1
+n2 = 2
+fib_array = Array.new
+fib_array << n1
+fib_array << n2
+prev = 2
+prev_prev = 1
+num = prev + prev_prev
+while num < 4000000
+	fib_array << num
+	prev_prev = prev
+	prev = num
+	num = prev + prev_prev
+end
+
+sum = 0
+for num in fib_array
+	sum += num if num%2 == 0 
+end
+
+puts sum
